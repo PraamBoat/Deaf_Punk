40afb262923ad0a1080ef37281b7ab38741d704c
Dav <davidcalsp@gmail.com>
2022-01-14 14:23:46 -0800

    Emulator now works, ready to begin work

--- app/src/main/java/com/example/fumolizer/MainActivity.kt
+++ app/src/main/java/com/example/fumolizer/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.fumolizer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.app.Activity
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttoner = findViewById(R.id.button_main_audioTest) as Button
+        buttoner.setOnClickListener {
+            Toast.makeText(this, "It works.", Toast.LENGTH_SHORT).show()
+        }
     }
+
+
+
 }
