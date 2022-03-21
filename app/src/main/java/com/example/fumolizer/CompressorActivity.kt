47ad8a008deacbd6e9542e5804c36e40fc0d1b27
Dav <davidcalsp@gmail.com>
2022-03-21 14:02:08 -0700

    Added the music bar to the CompressorActivity.kt as it was previously unused.

--- app/src/main/java/com/example/fumolizer/CompressorActivity.kt
+++ app/src/main/java/com/example/fumolizer/CompressorActivity.kt
@@ -3,7 +3,10 @@ package com.example.fumolizer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.app.Activity
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.view.View
 import android.view.View.OnClickListener
 import android.widget.Button
@@ -12,33 +15,102 @@ import android.media.audiofx.Equalizer
 import android.media.audiofx.AudioEffect
 import java.lang.Object
 import android.media.MediaPlayer
+import android.os.Build
 import android.os.Handler
+import android.widget.ImageButton
+import androidx.annotation.RequiresApi
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import java.lang.AssertionError
 
 class CompressorActivity : AppCompatActivity() {
 
+    lateinit var broadCastReceiver : BroadcastReceiver
+    var iF = IntentFilter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_compressor)
+
+        val barTitle = findViewById<Button>(R.id.button_compressor_title)
+
+        if (intent == null){
+            barTitle.text = "Error: Please change song"
+        }
+        else{
+            barTitle.text = intent.getStringExtra("barTitle")
+        }
+
+        iF.addAction("com.android.music.metachanged")
+        iF.addAction("com.htc.music.metachanged")
+        iF.addAction("fm.last.android.metachanged")
+        iF.addAction("com.sec.android.app.music.metachanged")
+        iF.addAction("com.nullsoft.winamp.metachanged")
+        iF.addAction("com.amazon.mp3.metachanged")
+        iF.addAction("com.miui.player.metachanged")
+        iF.addAction("com.real.IMP.metachanged")
+        iF.addAction("com.sonyericsson.music.metachanged")
+        iF.addAction("com.rdio.android.metachanged")
+        iF.addAction("com.samsung.sec.android.MusicPlayer.metachanged")
+        iF.addAction("com.andrew.apollo.metachanged")
+        iF.addAction("in.krosbits.musicolet")
+        iF.addAction("in.krosbits.musicolet.metachanged")
+        iF.addAction("AudioEffect.ACTION_OPEN_AUDIO_EFFECT_CONTROL_SESSION")
+        iF.addAction("AudioEffect.ACTION_CLOSE_AUDIO_EFFECT_CONTROL_SESSION")
+
+        broadCastReceiver = object : BroadcastReceiver() {
+            @RequiresApi(Build.VERSION_CODES.Q)
+            override fun onReceive(contxt: Context?, intent: Intent?) {
+
+                val newTitle = intent?.getStringExtra("track").toString()
+                barTitle.text = "Now Playing: $newTitle"
+
+            }
+        }
+
+        registerReceiver(broadCastReceiver, iF)
+
+        val nextButton = findViewById<ImageButton>(R.id.imageButton_compressor_next)
+        val backButton = findViewById<ImageButton>(R.id.imageButton_compressor_back)
+        val playButton = findViewById<ImageButton>(R.id.imageButton_compressor_play)
+
+        nextButton.setOnClickListener{
+            val intent = Intent(this, BackgroundSoundService::class.java)
+            intent.putExtra("action", "forward")
+            startService(intent)
+        }
+
+        backButton.setOnClickListener {
+            val intent = Intent(this, BackgroundSoundService::class.java)
+            intent.putExtra("action", "backwards")
+            startService(intent)
+        }
+
+        playButton.setOnClickListener {
+            val intent = Intent(this, BackgroundSoundService::class.java)
+            intent.putExtra("action", "playing")
+            startService(intent)
+        }
 
         findViewById<BottomNavigationView>(R.id.bottom_navigation).selectedItemId = R.id.ic_compressor
         findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnNavigationItemSelectedListener {
             when (it.itemId){
                 R.id.ic_volume -> {
                     val intent = Intent(this, VolumeActivity::class.java)
+                    intent.putExtra("barTitle", barTitle.text)
                     startActivity(intent)
                     finish()
                     true
                 }
                 R.id.ic_equalizer -> {
                     val intent = Intent(this, EqualizerActivity::class.java)
+                    intent.putExtra("barTitle", barTitle.text)
                     startActivity(intent)
                     finish()
                     true
                 }
                 R.id.ic_fumolizer -> {
                     val intent = Intent(this, MainActivity::class.java)
+                    intent.putExtra("barTitle", barTitle.text)
                     startActivity(intent)
                     finish()
                     true
@@ -48,6 +120,7 @@ class CompressorActivity : AppCompatActivity() {
                 }
                 R.id.ic_settings -> {
                     val intent = Intent(this, SettingsActivity::class.java)
+                    intent.putExtra("barTitle", barTitle.text)
                     startActivity(intent)
                     finish()
                     true
