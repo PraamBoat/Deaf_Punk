20add18f3422f4a05cae4d405860be7f2c606bd4
Dav <davidcalsp@gmail.com>
2022-03-21 14:08:33 -0700

    Minor fixes for merges.

--- app/src/main/java/com/example/fumolizer/SettingsActivity.kt
+++ app/src/main/java/com/example/fumolizer/SettingsActivity.kt
@@ -1,7 +1,6 @@
 package com.example.fumolizer
 
-import android.content.Intent
-import android.content.SharedPreferences
+import android.content.*
 import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
