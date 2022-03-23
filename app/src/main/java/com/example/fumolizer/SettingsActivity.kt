367b29e586399d0e61eff6bd63767d72cca760ea
kaipen <thisisnotfakemaybe@gmail.com>
2022-03-23 13:56:08 -0700

    color selector and shared preferecnes work only works on one button

--- app/src/main/java/com/example/fumolizer/SettingsActivity.kt
+++ app/src/main/java/com/example/fumolizer/SettingsActivity.kt
@@ -15,35 +15,51 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class SettingsActivity : AppCompatActivity() {
 
+    val SHARED_PREFS = "sharedPrefs"
+    val SAVERED = "savered"
+    val SAVEGREEN = "savegreen"
+    val SAVEBLUE = "saveblue"
+
+    var red: Float = 0F
+    var green: Float = 0F
+    var blue: Float = 0F
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-
+        val appSettingPrefs: SharedPreferences = getSharedPreferences( "AppSettingsPrefs", 0)
+        val sharedPrefsEdit: SharedPreferences.Editor = appSettingPrefs.edit()
+        val isNightModeOn: Boolean = appSettingPrefs.getBoolean( "NightMode", false)
         // TO-DO: Find a way to get the current song information here!
-
         var button = findViewById<Button>(R.id.button_settings_darkMode)
         var seekR = findViewById<SeekBar>(R.id.seekBar_settings_color)
         var seekG = findViewById<SeekBar>(R.id.seekBar_settings_colorG)
         var seekB = findViewById<SeekBar>(R.id.seekBar_settings_colorB)
 
         var nav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        var red: Float = 0F
-        var green: Float = 0F
-        var blue: Float = 0F
 
-        val appSettingPrefs: SharedPreferences = getSharedPreferences( "AppSettingsPrefs", 0)
-        val sharedPrefsEdit: SharedPreferences.Editor = appSettingPrefs.edit()
-        val isNightModeOn: Boolean = appSettingPrefs.getBoolean( "NightMode", false)
+        fun updateViews() {
+            button.setBackgroundColor(Color.parseColor(rgbtohex(red,green,blue)))
+            seekR.progress = (red / 2.55).toInt()
+            seekG.progress = (green / 2.55).toInt()
+            seekB.progress = (blue / 2.55).toInt()
+        }
+
+        loadData()
+        updateViews()
+
+
 
         if(isNightModeOn) {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
             button.text = "Disable Dark Mode"
-            Toast.makeText(this, "Enabled Dark Mode", Toast.LENGTH_SHORT).show()
+
         } else {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
             button.text = "Enable Dark Mode"
-            Toast.makeText(this, "Disabled Dark Mode", Toast.LENGTH_SHORT).show()
+
         }
         button.setOnClickListener {
             if(isNightModeOn) {
@@ -60,31 +76,7 @@ class SettingsActivity : AppCompatActivity() {
             }
         }
 
-        fun converthex(num:Int): String {
-            var list = listOf("0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F")
-            var yeet = list[num.toInt()]
-            return yeet
-        }
 
-        fun rgbtohex3(red:Float, green:Float, blue:Float):String{
-            var d1="0";var d3="0";var d5="0"
-            d1 = converthex((red/16).toInt())
-            d3 = converthex((green/16).toInt())
-            d5 = converthex((blue/16).toInt())
-            return "#" + d1 + d3 + d5
-        }
-
-        fun rgbtohex(red:Float, green:Float, blue:Float):String{
-            var d1="0";var d2="0";var d3="0";var d4="0";var d5="0";var d6="0";
-            d1 = converthex((red/16).toInt())
-            d2 = converthex(((red/16 - red/16.toInt())*16).toInt())
-            d3 = converthex((green/16).toInt())
-            d4 = converthex(((green/16 - green/16.toInt())*16).toInt())
-            d5 = converthex((blue/16).toInt())
-            d6 = converthex(((blue/16 - blue/16.toInt())*16).toInt())
-            return "#" + d1 + d2 + d3 + d4 + d5 + d6
-
-        }
 
        seekR.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
            override fun onProgressChanged(seekbar: SeekBar?, progress: Int, fromUser: Boolean) {
@@ -100,6 +92,7 @@ class SettingsActivity : AppCompatActivity() {
 
            override fun onStopTrackingTouch(seekbar: SeekBar?) {
                button.setBackgroundColor(Color.parseColor(rgbtohex(red,green,blue)))
+               saveData()
            }
        })
 
@@ -116,6 +109,7 @@ class SettingsActivity : AppCompatActivity() {
 
             override fun onStopTrackingTouch(seekbar: SeekBar?) {
                 button.setBackgroundColor(Color.parseColor(rgbtohex(red,green,blue)))
+                saveData()
             }
         })
 
@@ -132,8 +126,12 @@ class SettingsActivity : AppCompatActivity() {
 
             override fun onStopTrackingTouch(seekbar: SeekBar?) {
                 button.setBackgroundColor(Color.parseColor(rgbtohex(red,green,blue)))
+                saveData()
             }
         })
+
+
+
 
 
         findViewById<Button>(R.id.button_settings_title).setOnClickListener {
@@ -179,7 +177,53 @@ class SettingsActivity : AppCompatActivity() {
 
 
 
+
     }
+    fun converthex(num:Int): String {
+        var list = listOf("0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F")
+        var yeet = list[num.toInt()]
+        return yeet
+    }
+
+    fun rgbtohex3(red:Float, green:Float, blue:Float):String{
+        var d1="0";var d3="0";var d5="0"
+        d1 = converthex((red/16).toInt())
+        d3 = converthex((green/16).toInt())
+        d5 = converthex((blue/16).toInt())
+        return "#" + d1 + d3 + d5
+    }
+
+    fun rgbtohex(red:Float, green:Float, blue:Float):String{
+        var d1="0";var d2="0";var d3="0";var d4="0";var d5="0";var d6="0";
+        d1 = converthex((red/16).toInt())
+        d2 = converthex(((red/16 - red/16.toInt())*16).toInt())
+        d3 = converthex((green/16).toInt())
+        d4 = converthex(((green/16 - green/16.toInt())*16).toInt())
+        d5 = converthex((blue/16).toInt())
+        d6 = converthex(((blue/16 - blue/16.toInt())*16).toInt())
+        return "#" + d1 + d2 + d3 + d4 + d5 + d6
+
+    }
+    fun saveData() {
+        var sharedPreferences: SharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE)
+        var editor = sharedPreferences.edit();
+
+        editor.putFloat(SAVERED, red)
+        editor.putFloat(SAVEGREEN, green)
+        editor.putFloat(SAVEBLUE, blue)
+        editor.apply();
+
+    }
+
+    fun loadData() {
+        var sharedPreferences: SharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE)
+        red= sharedPreferences.getFloat(SAVERED, 12F)
+        green = sharedPreferences.getFloat(SAVEGREEN, 12F)
+        blue = sharedPreferences.getFloat(SAVEBLUE, 12F)
+
+    }
+
+
 
 }
 
